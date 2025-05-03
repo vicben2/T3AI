1886966a3be9eaf26ca6b045db216f09e74d4d38
vicben2 <vicbbbb2@proton.me>
2025-05-03 11:11:34 +0800

    functional game

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ grids.forEach(grid => {
 
 let firstPlay = true
 let gridsUsed = 0
+let gameOver = false
 
 
 function selectGrid(e, grid) {
@@ -46,32 +47,114 @@ function selectGrid(e, grid) {
 }
 
 
+let gridStr = "---------"
 function updateBoard(grid) {
+    if(gameOver) {
+        return
+    }
+
     const elem = document.getElementById(grid)
     if(elem.innerText !== "" && elem.classList.contains("gridHov") === false) {
         return
     }
 
+    const gridNum = grid.match(/\d/)[0]
+    const regex = new RegExp(`(?<=.{${gridNum}}).`)
+
     if(firstPlay) {
         elem.innerText = "O"
+        gridStr = gridStr.replace(regex, "O")
     }
     else {
         elem.innerText = "X"
+        gridStr = gridStr.replace(regex, "X")
     }
     firstPlay = !firstPlay
     gridsUsed += 1
 
     elem.classList.remove("gridHov")
-    checkForWin()
+    if(gridsUsed >= 5) {
+        gameOver = checkForWin()
+    }
     
-    if(gridsUsed === 9) {
+    if(gridsUsed === 9 || gameOver) {
+        gameOver = true
         showReset()
     }
 }
 
 
+const oWinStatesRegex = [
+    /OOO....../,
+    /...OOO.../,
+    /......OOO/,
+    /O..O..O../,
+    /.O..O..O./,
+    /..O..O..O/,
+    /O...O...O/,
+    /..O.O.O../
+]
+const xWinStatesRegex = [
+    /XXX....../,
+    /...XXX.../,
+    /......XXX/,
+    /X..X..X../,
+    /.X..X..X./,
+    /..X..X..X/,
+    /X...X...X/,
+    /..X.X.X../
+]
 function checkForWin() {
-    
+    let output = false
+    if(firstPlay) {
+        xWinStatesRegex.forEach((regex, index) => {
+            if(regex.test(gridStr)) {
+                switch(index) {
+                    case 0: colorWinCells([0,1,2]); break
+                    case 1: colorWinCells([3,4,5]); break
+                    case 2: colorWinCells([6,7,8]); break
+                    case 3: colorWinCells([0,3,6]); break
+                    case 4: colorWinCells([1,4,7]); break
+                    case 5: colorWinCells([2,5,8]); break
+                    case 6: colorWinCells([0,4,8]); break
+                    case 7: colorWinCells([2,4,6]); break
+                    default: console.error("Something went wrong.")
+                }
+                output = true
+                console.log(regex)
+                console.log(gridStr)
+            }
+        });
+    }
+    else {
+        oWinStatesRegex.forEach((regex, index) => {
+            if(regex.test(gridStr)) {
+                switch(index) {
+                    case 0: colorWinCells([0,1,2]); break
+                    case 1: colorWinCells([3,4,5]); break
+                    case 2: colorWinCells([6,7,8]); break
+                    case 3: colorWinCells([0,3,6]); break
+                    case 4: colorWinCells([1,4,7]); break
+                    case 5: colorWinCells([2,5,8]); break
+                    case 6: colorWinCells([0,4,8]); break
+                    case 7: colorWinCells([2,4,6]); break
+                    default: console.error("Something went wrong.")
+                }
+                output = true
+                console.log(regex)
+                console.log(gridStr)
+            }
+        });
+    }
+    return output
+}
+
+
+function colorWinCells(arr) {
+    arr.forEach(num => {
+        const elem = document.getElementById("g" + num)
+        elem.classList.add("gridWin")
+    });
 }
 
 
@@ -87,8 +170,11 @@ function showReset() {
 function resetBoard() {
     gridsUsed = 0
     firstPlay = true
+    gameOver = false
+    gridStr = "---------"
     grids.forEach(grid => {
         grid.innerText = ""
+        grid.classList.remove("gridHov", "gridWin")
     })
     resetButton.style.visibility = "hidden"
 }
